76f0f77006683a85925b1e3f0e60ebe5dd5a2d63
mun19508 <68123854+mun19508@users.noreply.github.com>
2023-08-25 18:46:27 -0600

    Update top.v

--- src/top.v
+++ src/top.v
@@ -1,6 +1,6 @@
 module tt_um_counter(
-    input  wire ui_in,    // Dedicated inputs
-    output wire uo_out,   // Dedicated outputs
+    input  wire [7:0] ui_in,    // Dedicated inputs
+    output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
     output wire [7:0] uio_out,  // IOs: Output path
     output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
