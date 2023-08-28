8af9470cfc08d04a4f86259bf5b9738ca075e8de
mun19508 <mun19508@uvg.edu.gt>
2023-08-28 11:35:08 -0600

    Agregando nuevo modulo de prueba

---
+++ src/top2.v
@@ -0,0 +1,19 @@
+`default_nettype none
+
+module tt_um_inverter #( parameter MAX_COUNT = 24'd10_000_000 ) (
+    input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
+    output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
+    input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
+    output wire [7:0] uio_out,  // IOs: Bidirectional Output path
+    output wire [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // will go high when the design is enabled
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
+);
+
+// Use bidirectional as output
+assign uio_oe = 8'b11111111;
+
+assign uo_out = ! clk;
+
+endmodule
