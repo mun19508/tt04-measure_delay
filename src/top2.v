5c15a529b5973dbc4e36facec544b345a6168fd3
mun19508 <mun19508@uvg.edu.gt>
2023-08-28 11:52:29 -0600

    Prueba 3

--- src/top2.v
+++ src/top2.v
@@ -12,8 +12,8 @@ module tt_um_inverter #( parameter MAX_COUNT = 24'd10_000_000 ) (
 );
 
 // Use bidirectional as output
-assign uio_oe = 8'b11111111;
+assign uio_oe =  8'b00000000;
 
-assign uio_out = ! clk;
+assign uo_out = ! clk;
 
 endmodule
